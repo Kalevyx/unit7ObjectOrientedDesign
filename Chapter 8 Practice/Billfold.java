4803f20b5398991b3b30d0b986b0bda750efdf5e
Kalevyx <crli@NN123-HA-07.HS.sd203.org>
2016-02-03 15:10:25 -0600

    February 3, 2016

--- Chapter 8 Practice/Billfold.java
+++ Chapter 8 Practice/Billfold.java
@@ -6,8 +6,16 @@ public class Billfold extends CardHierarchy{
         card2 = c2;}
     public void addCard(CardHierarchy card){
         if(card1==null){
-            card1=new CardHierarchy();}
+            card1=card;}
         else if(card2==null){
-            card2=new CardHierarchy();}}
+            card2=card;}}
     public String formatCards(){
-        return super.format();}}
+        String str = "";
+        if(card1!=null){
+            str+=card1.format();}
+        else if(card2!=null){
+            str+=card2.format();}
+        return str;}
+    public static void main(String[]args){
+        Billfold billfold = new Billfold();
+        billfold.addCard(new CardHierarchy(J));}}
--- Chapter 8 Practice/IDCard.java
+++ Chapter 8 Practice/IDCard.java
@@ -2,4 +2,14 @@ public class IDCard extends CardHierarchy{
     String idNumber;
     public IDCard(String n, String id){
         super(n);
-        idNumber = id;}}
+        idNumber = id;}
+    public boolean equals(Object other){
+        //check if this object and other object are of same class
+        if(this.getClass()==other.getClass()){
+            //if so, case other object to IDCard
+            IDCard otherIDCard = (IDCard) other;
+            //check if superclass (CardHierarchy) determines this object and other object
+            //are equal
+            boolean isEqual = super.equals(otherIDCard);
+            //check if instance variables of this object and other object are equal
+            return isEqual&&this.idNumber.equals(otherIDCard.idNumber);}}}
