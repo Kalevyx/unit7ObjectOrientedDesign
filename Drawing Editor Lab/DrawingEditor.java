ff02b7bfb984ecd3d90c1fb118872c565ccf4d1e
Kalevyx <kalevyx@gmail.com>
2016-03-04 23:51:42 -0600

    Update DrawingEditor.java

--- Drawing Editor Lab/DrawingEditor.java
+++ Drawing Editor Lab/DrawingEditor.java
@@ -1,17 +1,29 @@
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 
+/**
+ * The frame of the program
+ * 
+ * @author crli
+ * @date March 4, 2016
+ */
 public class DrawingEditor extends JFrame{
+    //Sets frame width
     private static final int FRAME_WIDTH = 500;
+    //Sets frame height
     private static final int FRAME_HEIGHT = 500;
-    
+    //Creates frame variable
     private JFrame frame;
+    //Creates canvas variable
     private DrawingPanel canvas;
+    //Creates controls variable
     private ControlPanel controls;
     
+    //Creates frame with controls and canvas
     public DrawingEditor(){
         canvas = new DrawingPanel();
         controls = new ControlPanel(canvas);
+        add(canvas, BorderLayout.CENTER);
         add(controls, BorderLayout.PAGE_END);
         
         setSize(FRAME_WIDTH,FRAME_HEIGHT);
@@ -19,5 +31,6 @@ public class DrawingEditor extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);}
 
+    //Creates visible frame and canvas where shapes are added
     public static void main(String[]args){
         DrawingEditor viewer = new DrawingEditor();}}
