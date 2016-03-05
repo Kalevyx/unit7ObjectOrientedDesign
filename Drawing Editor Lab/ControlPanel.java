d5e81bf36b5b6b6e8fdaf47bb58a865bd4a436f1
Kalevyx <kalevyx@gmail.com>
2016-03-04 23:51:20 -0600

    Update ControlPanel.java

--- Drawing Editor Lab/ControlPanel.java
+++ Drawing Editor Lab/ControlPanel.java
@@ -1,18 +1,29 @@
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Sets up panel and buttons
+ * Extends JPanel
+ * 
+ * @author crli
+ * @date March 4, 2016
+ */
 public class ControlPanel extends JPanel{
+    //Creates canvas variable
     private DrawingPanel canvas;
+    //Creates colorButton variable
     private JButton colorButton;
+    //Create circleButton variable
     private JButton circleButton;
+    //Creates squareButton variable
     private JButton squareButton;
-    private JLabel label;
+    //Creates panel variable
     private JPanel panel;
     
+    //Sets up canvas and adds buttons
     public ControlPanel(DrawingPanel c){
         canvas = c;
         this.colorButton = new JButton("Pick Color");
@@ -29,15 +40,17 @@ public class ControlPanel extends JPanel{
         this.colorButton.addActionListener(listener);
         this.circleButton.addActionListener(listener);
         this.squareButton.addActionListener(listener);}
-        
+    
+    //Each button with its respective abilities
+    //Chooses color or adds square or circle
     public class ClickListener implements ActionListener{
         public void actionPerformed(ActionEvent event){
             if(event.getSource()==colorButton){
                 canvas.pickColor();
                 panel.setBackground(canvas.getColor());}
             else if(event.getSource()==circleButton){
-                //add circle
+                canvas.addCircle();
             }
             else{
-                //add square
+                canvas.addSquare();
             }}}}
